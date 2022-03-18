980e4b353c0fcf9071f4dcfbb6d495064de650ac
Ryan Chang <ryanchang0501@gmail.com>
2022-03-17 22:39:52 -0400

    Initial commit

---
+++ app/src/main/java/com/rchang0501/rejuvenate/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rchang0501.rejuvenate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
