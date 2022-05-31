bdc28a04b75c96e3694ae1ad95f1815758f0c70c
Ryan Chang <ryanchang0501@gmail.com>
2022-05-30 21:30:43 -0400

    improved documentation in filter mode class

--- app/src/main/java/com/rchang0501/rejuvenate/reminderlistfragment/ReminderFilterMode.kt
+++ app/src/main/java/com/rchang0501/rejuvenate/reminderlistfragment/ReminderFilterMode.kt
@@ -1,5 +1,6 @@
 package com.rchang0501.rejuvenate.reminderlistfragment
 
+// different filter modes in the list fragment
 enum class ReminderFilterMode(val position: Int) {
     TODAY(0), FUTURE(1), ALL(2)
 }
